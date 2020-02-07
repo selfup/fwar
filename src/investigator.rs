54057cc05ffecf9474409279ed7dfe0de639015f
selfup <boudinot.regis@yahoo.com>
2020-02-06 23:32:32 -0600

    walkdir and rayon initial use

--- src/investigator.rs
+++ src/investigator.rs
@@ -4,10 +4,11 @@
 
 use std::fs;
 use std::io::{Error, ErrorKind, Result};
+use std::path::Path;
 use std::time::{Duration, SystemTime};
 
-pub fn investigate() -> Result<Duration> {
-    let metadata = fs::metadata("artifact/wow.txt")?;
+pub fn investigate(path: &Path) -> Result<Duration> {
+    let metadata = fs::metadata(path)?;
 
     match metadata.modified() {
         Ok(modifed_time) => match modifed_time.duration_since(SystemTime::UNIX_EPOCH) {
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,23 @@
 
 mod investigator;
 
+use rayon::prelude::*;
+use std::io::Result;
+use std::time::Duration;
+use walkdir::WalkDir;
+
 fn main() {
-    match investigator::investigate() {
-        Ok(result) => println!("{:?}", result),
-        Err(err) => println!("{:?}", err),
+    let mut files = vec![];
+    let mut timestamps: Vec<Result<Duration>> = vec![];
+
+    for entry in WalkDir::new("artifact").into_iter().filter_map(|e| e.ok()) {
+        files.push(entry);
     }
+
+    files
+        .par_iter()
+        .map(|file| investigator::investigate(file.path()))
+        .collect_into_vec(&mut timestamps);
+
+    println!("{:?}", timestamps);
 }
